4968c303b041cf7370eb4b8baef113f8c1138aac
Sheldon Burks <sheldon.burks@gmail.com>
2016-11-13 12:55:33 -0500

    Started adding models and fixed regex

--- open_shop.py
+++ open_shop.py
@@ -1,5 +1,19 @@
 import argparse, re
 
+class Machine(object):
+    def __init__(self, power_factor):
+        self.power_factor = power_factor
+
+
+class Job(object):
+    def __init__(self, time):
+        self.process_time = time
+
+class Instance(object):
+    def __init__(self, machines, jobs):
+        self.machines = machines
+        self.jobs = jobs
+
 def parse_args():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-f', '--file',
@@ -8,9 +22,11 @@ def parse_args():
 
 def parse_file(file_path):
     with open(file_path) as file:
-        contents = file.readline()
-        print(contents)
-        machines = re.match(r'([0-9]*[.][0-9]*)', contents)
+        contents = file.read()
+        machines = re.findall(r'MACHINE: ([0-9. ]*)', contents)[0].split(' ')
+        jobs = re.findall(r'JOBS: ([0-9 ]*)', contents)[0].split(' ')
+        print(machines)
+        print(jobs)
 
 if __name__ == '__main__':
     args = parse_args()
