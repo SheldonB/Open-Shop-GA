4e58948cfbe656eff05e85102fb24edfbd3734de
Sheldon Burks <sheldon.burks@gmail.com>
2016-11-20 15:59:32 -0500

    Redid how GA worked. Now using a sequence of ints as a chromosome

--- open_shop.py
+++ open_shop.py
@@ -1,203 +1,129 @@
-import argparse, re, copy, random, math
-
+import argparse, re, random, math, copy
 from operator import attrgetter
 
-class Machine(object):
-    def __init__(self, id, power_factor):
-        self.id = id
-        self.power_factor = power_factor
+POPULATION = 0
+MUTATION = 0
+GENERATIONS = 0
+GROUP = 0
 
-    def __repr__(self):
-        return '{}: {}'.format(self.id, self.power_factor)
+lookup_table = None
 
-class Job(object):
-    def __init__(self, id, time):
-        self.id = id
-        self.process_time = time
+class Chromosome(object):
+    """
+    The chromosome is a representation
+    of the genetic chromosome that is used
+    to develop a schedule and calculate the
+    makespan.
 
-    def __repr__(self):
-        return '{}: {}'.format(self.id, self.process_time)
+    The chromosome is represented as an array
+    that is of size m * n, where m is the number
+    of machines, and n is the number of jobs.
 
-class Instance(object):
-    def __init__(self, machine, job):
-        self.machine = machine
-        self.job = job
+    Each operation is assigned a unique identifier
+    with its cost. So Job 1 on Machine 1 would have
+    an identifier of 1.
 
-    def __repr__(self):
-        return '(Machine: {}, Job:{})'.format(self.machine.id, self.job.id)
+    The array represents the order in which operations
+    are scheduled.
 
-class Schedule(object):
-    def __init__(self, matrix):
-        self.matrix = matrix
+    A possible Chromosome might look like the following
+    if there are 3 jobs and 3 machines
+
+    [7, 5, 9, 3, 1, 8, 6, 2, 4]
+
+    This means that schedule operation 7 first, then
+    schedule operation 5, then schedule operation 9, and
+    so on.
+    """
+    def __init__(self, sequence):
+        self.sequence = sequence
         self._makespan = None
 
     @property
     def makespan(self):
-        if self._makespan is None:
-            job_time = [0 for i in range(len(self.matrix[0]))]
-            machine_time = [0 for i in range(len(self.matrix))]
+        schedule = [[0 for j in lookup_table[0]] for i in lookup_table]
 
-            for i in range(len(self.matrix[0])):
-                for j in range(len(self.matrix)):
-                    machine = self.matrix[j][i].machine
-                    job = self.matrix[j][i].job
+        for operation in self.sequence:
+            job = id_to_job_index(operation)
+            machine = id_to_machine_index(operation)
+            op_cost = id_to_lookup(operation)
 
-                    if job_time[job.id - 1] > machine_time[machine.id - 1]:
-                        machine_time[machine.id - 1] = job_time[job.id - 1] + \
-                                (machine.power_factor * job.process_time)
-                    else:
-                        machine_time[machine.id - 1] = machine_time[machine.id - 1] + \
-                                (machine.power_factor * job.process_time)
+            column = [row[machine] for row in schedule]
+            next_time = max(column)
 
-                    job_time[job.id - 1] = machine_time[machine.id - 1]
+            if max(schedule[job]) > next_time:
+                next_time = max(schedule[job])
 
-            self._makespan = max(machine_time)
+            schedule[job][machine]  = next_time + op_cost
 
-        return self._makespan
-
+        columns = zip(*schedule)
+        makespan = 0
+        for column in columns:
+            cand = max(column)
+            if cand > makespan:
+                makespan = cand
+        return makespan
 
 class Population(object):
-    def __init__(self, machines, jobs, size):
-        self.population_size = size
+    def __init__(self, size):
+        self.size = size
+        self._members = []
+        self._seed_population()
 
-        self.members = self._seed_population(machines, jobs)
-
-    def _seed_population(self, machines, jobs):
-        members = []
-        # Generate a Latin Square Matrix. That has no repeating
-        # Columns and rows.
-        schedule = []
-        for i in range(0, len(machines)):
-            row = [Instance(machines[i], job) for job in jobs]
-            for j in range(0, i):
-                to_rotate = row.pop(0)
-                row.append(to_rotate)
-            schedule.append(row)
-        members.append(Schedule(schedule))
-
-        # To generate random members for the population
-        # We can just swap the columns of the population
-        for i in range(1, self.population_size):
-            s_copy = copy.deepcopy(schedule)
-            (rand_col_1, rand_col_2) = random.sample(range(0, len(jobs) - 1) , 2)
-            for row in s_copy:
-                row[rand_col_1], row[rand_col_2] = row[rand_col_2], row[rand_col_1]
-            members.append(Schedule(s_copy))
-
-        return members
-
-    def _crossover(self, parent_one, parent_two):
-        """
-        Crossover method for the genetic algorithm.
-        The method takes two members of the population
-        and returns a child member of the population.
-        """
-        # Initialze the child matrix
-        child_matrix = [[ 0 for j in range(len(parent_one.matrix[0]))]
-                                for i in range(len(parent_one.matrix))]
-
-        # Choose random indicies for crossover
-        (rand_col_1, rand_col_2) = random.sample(range(0, len(parent_one.matrix[0])) , 2)
-
-        # If random column 1 is bigger then
-        # random column 2, then swap them
-        # just to make things easier
-        if rand_col_1 > rand_col_2:
-            rand_col_1, rand_col_2 = rand_col_2, rand_col_1
-
-        # Fill the columns of the child with the columns
-        # of parent one between the two indicies
-        for i in range(rand_col_1, rand_col_2):
-            for j, row in enumerate(parent_one.matrix):
-                child_matrix[j][i] = row[i]
-
-        # Transpose the matrix of parent two, so we
-        # now have an array of all columns, that can
-        # be compared to the columns
-        parent_two_columns = list(map(list, zip(*parent_two.matrix)))
-
-        # I am not a fan of this, but it converts the child
-        # matrix to a reduced column matrix of just job numbers
-        child_matrix_columns = []
-        for column in zip(*child_matrix):
-            if column[0] == 0:
-                child_matrix_columns.append(list(column))
-            else:
-                child_matrix_columns.append([i.job.id for i in column])
-
-        for cand_column in parent_two_columns:
-            r_cand_column = [i.job.id for i in cand_column]
-
-            if r_cand_column not in child_matrix_columns:
-                for i in range(len(child_matrix[0])):
-                    if child_matrix[0][i] == 0:
-                        for j in range(len(child_matrix)):
-                            child_matrix[j][i] = cand_column[j]
-                        break
-
-        return Schedule(child_matrix)
-
-    def _mutate(self, member):
-        """
-        Mutation method for the genetic algorithm.
-        The method takes a member of the population
-        that is chosen based on the mutation rate.
-
-        That member is then mutated, and a new member
-        is returned
-        """
-        (index_one, index_two) = random.sample(range(0, len(member.matrix)), 2)
-        for row in member.matrix:
-            row[index_one], row[index_two] = row[index_two], row[index_one]
-
-
-        # temp_id_1 = member.matrix[index_one][0].machine.id
-        # temp_id_2 = member.matrix[index_two][0].machine.id
-        # member.matrix[index_one], member.matrix[index_two] = member.matrix[index_two], \
-                                                             # member.matrix[index_one]
-
-        # for i in range(len(member.matrix)):
-            # member.matrix[index_one][i].machine.id = temp_id_1
-            # member.matrix[index_two][i].machine.id = temp_id_2
-        member._makespan = None
+    def _seed_population(self):
+        sequence_size = len(lookup_table) * len(lookup_table[0])
+        for i in range(self.size):
+            sequence = random.sample(range(1, sequence_size + 1), sequence_size)
+            self._members.append(Chromosome(sequence))
 
     def evolve_population(self):
-        """
-        Evolve population will run generation of the
-        genetic algorithm.
-        """
         (parent_one, parent_two) = self._selection()
-
         child = self._crossover(parent_one, parent_two)
-
-        self.members.append(child)
-
-        for member in self.members:
+        self._members.append(child)
+        self.kill_weak()
+        for member in self._members:
             if random.random() < MUTATION:
                 self._mutate(member)
-        self.kill_weak()
-        fit = self.fittest(1)[0]
-        for row in fit.matrix:
-            print([i.job.id for i in row])
-        print(fit.makespan)
-        # fit = sorted(self.members, key=attrgetter('makespan'))[0]
+    
+    def _crossover(self, parent_one, parent_two):
+        (start_index, end_index) = random.sample(range(len(parent_one.sequence)), 2)
+        
+        child_seq = [None] * len(parent_one.sequence)
+        
+        for i in range(len(child_seq)):
+            if start_index < end_index and i >= start_index and i <= end_index:
+                child_seq[i] = parent_two.sequence[i]
+            elif start_index > end_index:
+                if not (i <= start_index and i >= end_index):
+                    child_seq[i] = parent_one.sequence[i]
 
-        # weak = sorted(self.members, key=attrgetter('makespan'))[-1]
-        # print(fit.makespan, weak.makespan)
+        for i in range(len(child_seq)):
+            if parent_two.sequence[i] not in child_seq:
+                for j in range(len(child_seq)):
+                    if child_seq[j] is None:
+                        child_seq[j] = parent_two.sequence[i]
+                        break
+
+        return Chromosome(child_seq)
+
+    def _mutate(self, member):
+        (index_one, index_two) = random.sample(range(len(member.sequence)), 2)
+
+        member.sequence[index_one], member.sequence[index_two] = member.sequence[index_two], member.sequence[index_one]
 
     def _selection(self):
-        num_to_select = math.floor(len(self.members) * (GROUP/100))
-        sample = random.sample(range(0, len(self.members)), num_to_select)
+        num_to_select = math.floor(self.size * (GROUP/100))
+        sample = random.sample(range(self.size), num_to_select)
 
-        sample_members = sorted([self.members[i] for i in sample], key=attrgetter('makespan'))
+        sample_members = sorted([self._members[i] for i in sample], key=attrgetter('makespan'))
         return sample_members[:2]
 
     def fittest(self, size):
-        return sorted(self.members, key=attrgetter('makespan'))[:size]
+        return sorted(self._members, key=attrgetter('makespan'))[:size]
 
     def kill_weak(self):
-        weakest = max(self.members, key=attrgetter('makespan'))
-        self.members.remove(weakest)
+        weakest = max(self._members, key=attrgetter('makespan'))
+        self._members.remove(weakest)
 
 def parse_args():
     arg_parser = argparse.ArgumentParser()
@@ -221,24 +147,64 @@ def parse_file(file_path):
     """
     with open(file_path) as file:
         contents = file.read()
-        machines = re.findall(r'MACHINE: ([0-9. ]*)', contents)[0].split(' ')
-        jobs = re.findall(r'JOBS: ([0-9 ]*)', contents)[0].split(' ')
+        machines = list(map(float, re.findall(r'MACHINE: ([0-9. ]*)', contents)[0].split(' ')))
+        jobs = list(map(int, re.findall(r'JOBS: ([0-9 ]*)', contents)[0].split(' ')))
 
-        machines = [Machine(index+1, float(i)) for index, i in enumerate(machines)]
-        jobs = [Job(index+1, int(i)) for index, i in enumerate(jobs)]
+        return (machines, jobs)
 
-        return {
-            'machines': machines,
-            'jobs': jobs
-            }
+def id_to_machine_index(id):
+    transposed_id = id - 1
+    return transposed_id % len(lookup_table)
+
+def id_to_job_index(id):
+    transposed_id = id - 1
+    return transposed_id // len(lookup_table[0])
+
+def id_to_lookup(id):
+    """
+    Looks up the value in the lookup table
+    based on a unique identifier assigned to
+    job and machine.
+
+    For example, the Operation Job 1, Machine 1 would 
+    have an id of 1. This would look that value up.
+    """
+    transposed_id = id - 1
+    return lookup_table[transposed_id//len(lookup_table)][transposed_id%len(lookup_table[0])]
+
+def create_lookup_table(machines, jobs):
+    """
+    Create a lookup table to lookup what each
+    job will cost on a specific machine.
+
+    The jobs are the rows, and the machines are the columns
+
+    For example: To find the operation cost to perform
+    job 1 on machine 1, you would access the data held
+    in the lookup table at index [0][0]. To find the
+    operation cost to run job 2 on machine 3, you would
+    access the data held in the lookup table [1][2]
+
+    Note: The index is of the machine and job is always
+    one less then the ID of the job/machine
+    """
+    global lookup_table
+
+    lookup_table = [[0 for i in jobs ] for i in machines]
+
+    for i in range(0, len(machines)):
+        for j in range(0, len(jobs)):
+            lookup_table[j][i] = machines[i] * jobs[j]
 
 def start_ga(population):
+    best = 999999999
     for i in range(GENERATIONS):
         population.evolve_population()
+        fittest = population.fittest(1)[0]
+        if fittest.makespan < best:
+            best = fittest.makespan 
+        print(best)
 
-MUTATION = 0
-GENERATIONS = 0
-GROUP = 0
 
 if __name__ == '__main__':
     args = parse_args()
@@ -246,8 +212,9 @@ if __name__ == '__main__':
     MUTATION = args.mutation
     GENERATIONS = args.generations
     GROUP = args.group
-    data = parse_file(args.file)
 
-    population = Population(data['machines'], data['jobs'], POPULATION)
+    machines, jobs = parse_file(args.file)
+    create_lookup_table(machines, jobs)
+
+    population = Population(POPULATION)
     start_ga(population)
-
